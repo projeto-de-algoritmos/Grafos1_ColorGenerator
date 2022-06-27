d59fe78ff2dab56c9072ad6a5d225ab27a4522e6
isadoragalvaoss <isadoragalvaoss@gmail.com>
2022-06-27 20:04:24 -0300

    update rgb colors
    Co-authored-by: Luís Furtado <luiscesm1@gmail.com>

--- src/scripts/mock-colors.js
+++ src/scripts/mock-colors.js
@@ -1,24 +1,7 @@
-
 export const resultColors = [
-  [   "Laranja", 
-      ["Amarelo", "Vermelho"],
-      "add cor rgb aqui",
-  ],
-
-  [
-      "Verde", 
-      ["Azul", "Amarelo"],
-      "add cor rgb aqui",
-  ],
-  [
-      "Roxo", 
-      ["Vermelho", "Azul"],
-      "add cor rgb aqui",
-  ],
+  ["Laranja", ["Amarelo", "Vermelho"], "rgb(255, 165, 0)"],
+  ["Verde", ["Azul", "Amarelo"], "rgb(0,128,0)"],
+  ["Roxo", ["Vermelho", "Azul"], "rgb(153, 51, 153)"],
 ];
 
-export const primaryColors = [
-  "Vermelho", 
-  "Amarelo",
-  "Azul",
-];
+export const primaryColors = ["Vermelho", "Amarelo", "Azul"];
