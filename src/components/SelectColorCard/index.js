55514ad58ce116d1cba90547417d6447b06ab220
isadoragalvaoss <isadoragalvaoss@gmail.com>
2022-06-27 19:59:41 -0300

    feat: create SelectColorCard component
    Co-authored-by: Luís Furtado <luiscesm1@gmail.com>

--- src/components/SelectColorCard/index.js
+++ src/components/SelectColorCard/index.js
@@ -1,43 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import { SelectColorCardButton, Image, Text } from './Style';
+import React, { useState, useEffect } from "react";
+import ColorCard from "../ColorCard";
+import { SelectColorCardButton, Image, Text } from "./Style";
 
-const SelectColorCard = ({
- title, addItem, removeItem
-}) => {
-  
+const SelectColorCard = ({ title, addItem, removeItem }) => {
   const [selected, setSelected] = useState(false);
 
-  useEffect(() => {
-  },[selected])
+  useEffect(() => {}, [selected]);
 
   return (
     <div>
-      { selected
-        ? (
-          <SelectColorCardButton
-            onClick={() => { 
-              setSelected(!selected); 
-              removeItem(title[0]); 
-            }}
-            chosen
-          >
-            <Text>
-              {title[0]}
-            </Text>
-          </SelectColorCardButton>
-        ) : (
-          <SelectColorCardButton 
-            onClick={() => { 
-              setSelected(!selected); 
-              addItem(title[0]); 
+      {selected ? (
+        <SelectColorCardButton
+          onClick={() => {
+            setSelected(!selected);
+            removeItem(title[0]);
+          }}
+          chosen
+          color={title[1]}
+        >
+          <div
+            style={{
+              backgroundColor: title[1],
+              width: "100%",
+              height: "80%",
+              color: "transparent",
             }}
           >
-            <Text>
-              {title[0]}
-            </Text>
-          </SelectColorCardButton>
-        )
-        }
+            <span>color</span>{" "}
+          </div>
+          <Text>
+            <ColorCard title={title} selected={selected} />
+          </Text>
+        </SelectColorCardButton>
+      ) : (
+        <SelectColorCardButton
+          onClick={() => {
+            setSelected(!selected);
+            addItem(title[0]);
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: title[1],
+              width: "100%",
+              height: "80%",
+              color: "transparent",
+            }}
+          >
+            <span>color</span>{" "}
+          </div>
+          <Text>
+            <ColorCard title={title} selected={selected} />
+          </Text>
+        </SelectColorCardButton>
+      )}
     </div>
   );
 };
