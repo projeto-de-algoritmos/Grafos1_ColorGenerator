a434b74d91e63afeadeb4976ebcb59cea463788d
isadoragalvaoss <isadoragalvaoss@gmail.com>
2022-06-27 19:54:20 -0300

    style: header component
    Co-authored-by: Luís Furtado <luiscesm1@gmail.com>

--- src/screens/header.js
+++ src/screens/header.js
@@ -1,19 +1,13 @@
 import React from "react";
 import logo from "../imgs/logo.png";
+import { HeaderDiv } from "./Style";
 
 const Header = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        marginBottom: "2%",
-      }}
-    >
+    <HeaderDiv>
       <img src={logo} width="180px" height="150px" />
       <h1>Seja bem vindo ao ColorGenerator</h1>
-    </div>
+    </HeaderDiv>
   );
 };
 
