c1b5a55bd057119acad22ebd02c88ecca5925bd9
isadoragalvaoss <isadoragalvaoss@gmail.com>
2022-06-27 20:03:11 -0300

    style: home component
    Co-authored-by: Luís Furtado <luiscesm1@gmail.com>

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
-import ColorCard from '../components/ColorCard/index';
-import SelectColorCard from '../components/SelectColorCard/index';
-import mixColors from '../scripts/index';
+import React, { useEffect, useState } from "react";
+import ColorCard from "../components/ColorCard/index";
+import SelectColorCard from "../components/SelectColorCard/index";
+import mixColors from "../scripts/index";
+import Header from "./header";
+import { FlexDiv } from "./Style";
 
 const colors = [
   ["Vermelho", "rgb(226, 1, 1)"],
@@ -19,75 +21,134 @@ const HomeScreen = () => {
   };
 
   const removeColor = (title) => {
-    for(let i = 0; i < selectedColors.length; i++){
-      if(selectedColors[i] === title){
-        selectedColors[i] = selectedColors[selectedColors.length-1];
-        selectedColors[selectedColors.length-1] = title;
-        selectedColors.pop(); 
+    for (let i = 0; i < selectedColors.length; i++) {
+      if (selectedColors[i] === title) {
+        selectedColors[i] = selectedColors[selectedColors.length - 1];
+        selectedColors[selectedColors.length - 1] = title;
+        selectedColors.pop();
       }
     }
   };
 
   const showResultOfMixColors = (mixs) => {
-    if(mixs.length > 0){
-      return (
-        mixs.map((type) => (
-          <ColorCard
-            key={Math.random()}
-            title={type}
-          />
-        ))
-      )
+    if (mixs.length > 0) {
+      return mixs.map((type) => (
+        <ColorCard key={Math.random()} title={type} node />
+      ));
     } else {
       return (
-        <div>
+        <FlexDiv>
           Não foi encontrado em nossos registros alguma cor resultante.
-        </div>
-      )
+        </FlexDiv>
+      );
     }
-  }
+  };
 
+  useEffect(() => {
+    console.log(selectedColors);
+  }, [selectedColors, mixState]);
+
+  const mixes = (selectedColors) => {
+    console.log(selectedColors);
+    setMixs(mixColors(selectedColors));
+    setMixState(true);
+  };
+
+  const notMix = () => {
+    alert("Selecione somente duas cores");
+    window.location.reload();
+  };
 
   return (
-    <div>    
-      { mixState 
-      ? <div>
-          A cor gerada foi:
+    <div style={{ backgroundColor: "#E8ECF3", height: "100vh" }}>
+      <div>
+        <Header />
+      </div>
+      {mixState ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h3>A cor gerada foi:</h3>
         </div>
-      : <div>
-          <div>
-            Seja bem vindo ao ColorGenerator
-          </div>
-          <div>
-            Selecione duas cores e verifique o resultado
+      ) : (
+        <div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <h3>Selecione duas cores e verifique o resultado</h3>
           </div>
         </div>
-      }
+      )}
 
-        { mixState
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {mixState
           ? showResultOfMixColors(mixs)
           : colors.map((type, i) => (
-            // permitir o componente aceitar selecionar 2 cores
-            <SelectColorCard
-              key={i}
-              title={type}
-              addItem={addColor}
-              removeItem={removeColor}
-            />
-          ))
-          }
+              // permitir o componente aceitar selecionar 2 cores
+              <SelectColorCard
+                key={i}
+                title={type}
+                addItem={addColor}
+                removeItem={removeColor}
+              />
+            ))}
+      </div>
 
-      { mixState
-        ? <div onClick={() => { setMixState(false); setSelectedColors([])}}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {mixState ? (
+          <div
+            onClick={() => {
+              setMixState(false);
+              setSelectedColors([]);
+            }}
+            style={{
+              border: "1px solid black",
+              margin: "50px",
+              padding: "10px",
+              cursor: "pointer",
+            }}
+          >
             Voltar
           </div>
-        : <div onClick={() => { setMixs(mixColors(selectedColors)); setMixState(true); }}>
+        ) : (
+          <div
+            onClick={() => {
+              selectedColors.length > 2 ? notMix() : mixes(selectedColors);
+            }}
+            style={{
+              border: "1px solid black",
+              margin: "50px",
+              padding: "10px",
+              cursor: "pointer",
+            }}
+          >
             {/* liberar o botão de confirmar apenas quando a quantidade de ingredientes selecionados for 2 */}
             Confirmar
           </div>
-      }
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default HomeScreen;
