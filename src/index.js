29e939093053f340407a525187e0ac74e49f9136
Luís Furtado <luiscesm1@gmail.com>
2022-06-27 12:58:57 -0300

    feat: create base project structure

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; 
+import HomeScreen from './screens/home';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <HomeScreen />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
