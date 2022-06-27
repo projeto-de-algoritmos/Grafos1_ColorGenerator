5378bb0ab087051d7ddc8750b9df1542e6945698
isadoragalvaoss <isadoragalvaoss@gmail.com>
2022-06-27 19:57:00 -0300

    feat: create ColorCard component
    Co-authored-by: Luís Furtado <luiscesm1@gmail.com>

--- src/components/ColorCard/Style.js
+++ src/components/ColorCard/Style.js
@@ -1,1 +1,22 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+
+export const Text = styled.p`
+  text-align: center;
+  margin: 20px;
+`;
+
+export const SelectColorCardButton = styled.div`
+  background-color: 'white'
+  color: ${(props) => (props.chosen ? "white" : "black")};
+  padding: 5px;
+  border-radius: 5px;
+  width: 15vw;
+  height: 18vw;
+  border-radius:2%;
+  margin: 3vh 1.5vh 0px 1.5vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  
+`;
--- src/components/ColorCard/index.js
+++ src/components/ColorCard/index.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React from "react";
+import { SelectColorCardButton, Text } from "./Style";
+import { AiOutlineCheck } from "react-icons/ai";
 
-const ColorCard = ({title}) => {
-    const colorContent = title;
-    return (
-        <div>
-            {colorContent.name}
-            {/* TODO: Fazer card representando cor de maneira agradavel */}
-        </div>
-    );
+const ColorCard = ({ title, node, selected }) => {
+  const colorContent = title[0];
+  const color = title[1];
+  console.log(title);
+  return (
+    <div>
+      <div>
+        {node ? (
+          <SelectColorCardButton chosen color={title[1]}>
+            <div
+              style={{
+                backgroundColor: title.color,
+                width: "100%",
+                height: "80%",
+                color: "transparent",
+              }}
+            >
+              <span>color</span>{" "}
+            </div>
+            <Text>{title.name}</Text>
+          </SelectColorCardButton>
+        ) : (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {selected && <AiOutlineCheck />}
+            <Text>{colorContent}</Text>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ColorCard;
